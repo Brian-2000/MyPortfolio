733479b5b02c5aff053bba0dbaa08d930e366738
Brian-2000 <mucherebrian@gmail.com>
2024-05-18 22:24:06 +0300

    added whitenoise

--- env/portfolio/portfolio/urls.py
+++ env/portfolio/portfolio/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.urls import re_path
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('portfolioapp.urls'))
+    path("", include('portfolioapp.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
